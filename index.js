f9f48eb4bad2006a7dccfce3c6c08da834c3ffee
Mouaz Jamal <mj225ih@student.lnu.se>
2025-11-20 22:01:49 +0100

    Creating a func for the whole project firstly to take the value and check if its max 8 and between 0-1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const binaryString = document.getElementById('input')
+
+const validating = () => {
+const value = binaryString.value
+if (value.length < 9) {
+    alert('Va vänlig och skriv minst 9 siffror 😀');
+        throw new TypeError('För få siffror.');
+}
+for (let i = 0; i < value.length; i++) {
+    if (value[i] !== '0' && value[i] !== '1') {
+        alert('Endast 0 och 1 är tillåtna!');
+        throw new TypeError('Felaktiga siffror i binär sträng.');
+    }
+}
+}
+
